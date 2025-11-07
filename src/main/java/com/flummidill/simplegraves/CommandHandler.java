63c57908c89502e615233c549308d100ae89211d
Flummidill <flummibrummbaer@web.de>
2025-11-08 00:02:53 +0100

    1.3.0 - Changed Update Checker to use ModrinthAPI and Added More Configurable Permissions

--- src/main/java/com/flummidill/simplegraves/CommandHandler.java
+++ src/main/java/com/flummidill/simplegraves/CommandHandler.java
@@ -162,6 +162,8 @@ public class CommandHandler implements CommandExecutor {
                     return false;
                 }
             }
+        } else {
+            // No Number
         }
 
         UUID targetUUID = UUID.fromString("00000000-0000-0000-0000-000000000000");
